309a18cd663768178317b01321b71631ae56479a
NicoDiLullo <ndilullo@thacher.org>
2024-10-21 18:04:00 -0400

    multiple folder

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-const folderContainer = document.getElementById("folder-container");
-
-folderContainer.addEventListener("mousedown", function(e) {
-  let offsetX = e.clientX - folderContainer.offsetLeft;
-  let offsetY = e.clientY - folderContainer.offsetTop;
-
-  function moveAt(event) {
-    folderContainer.style.left = event.clientX - offsetX + 'px';
-    folderContainer.style.top = event.clientY - offsetY + 'px';
-  }
-
-  function stopDrag() {
-    document.removeEventListener('mousemove', moveAt);
-    document.removeEventListener('mouseup', stopDrag);
-  }
-
-  document.addEventListener('mousemove', moveAt);
-  document.addEventListener('mouseup', stopDrag);
-});
-
-const folder = document.getElementById("folder");
-folder.addEventListener("dblclick", function() {
-  window.location.href = "https://example.com"; // URL of the page you want to navigate to
-});
+document.querySelectorAll('.folder-container').forEach(folderContainer => {
+    folderContainer.addEventListener('mousedown', function(e) {
+      let offsetX = e.clientX - folderContainer.offsetLeft;
+      let offsetY = e.clientY - folderContainer.offsetTop;
+  
+      function moveAt(event) {
+        folderContainer.style.left = event.clientX - offsetX + 'px';
+        folderContainer.style.top = event.clientY - offsetY + 'px';
+      }
+  
+      function stopDrag() {
+        document.removeEventListener('mousemove', moveAt);
+        document.removeEventListener('mouseup', stopDrag);
+      }
+  
+      document.addEventListener('mousemove', moveAt);
+      document.addEventListener('mouseup', stopDrag);
+    });
+  
+    const folder = folderContainer.querySelector('.folder');
+    folder.addEventListener('dblclick', function() {
+      window.location.href = "https://example.com"; // URL of the page you want to navigate to
+    });
+  });
+  
