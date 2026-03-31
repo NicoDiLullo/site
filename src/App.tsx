fb01d0743d94b9dfa454e4d6fb4d52b6e3a7aef9
NicoDiLullo <ndilullo@thacher.org>
2026-03-31 13:25:22 -0400

    idk

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App(): JSX.Element {
+  return (
+    <div className="app">
+      <h1>Hello, world!</h1>
+      <p>This is a React + TypeScript + Vite app.</p>
+    </div>
+  )
+}
